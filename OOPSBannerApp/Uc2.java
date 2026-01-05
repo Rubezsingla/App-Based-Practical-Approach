f44ad5eb9c08da6b03c34b6c4f686f50c1761dd0
Rubezsingla <124330683+Rubezsingla@users.noreply.github.com>
2026-01-05 21:28:47 +0530

    Added OOPSBannerApp folder

---
+++ OOPSBannerApp/Uc2.java
@@ -0,0 +1,33 @@
+public class Uc2 {
+
+
+    static class Banner2 {
+        void display2() {
+            String[] lines = {
+                    "  *     *     **       ***",
+                    " * *   * *    *    *   **",
+                    "*   * *   *   *     * **",
+                    "*   * *   *   *    *   **",
+                    "*   * *   *   **        *",
+                    "*   * *   *   *               *",
+                    "*   * *   *   *                *",
+                    " * *   * *    *               *",
+                    "  *     *     *          **"
+            };
+
+
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Banner2 b2 = new Banner2();
+        b2.display2();
+
+
+    }
+}
